40789be06788b5a58a4a66188fc13710c0eb2b65
Kody Samaroo <github email address>
2020-12-17 16:20:37 -0500

    commit tree 1ec2307de62728a0676b491e44e16609bb28c3d1

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 27, 'M', 'brown', 'happy', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 18, 'M', 'black', 'rowdy', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 21, 'F', 'brown', 'jolly', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 14, 'M', 'brown', 'aggro', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 9, 'F', 'polar', 'jolly', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 26, 'M', 'polar', 'aggro', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 18, 'F', 'black', 'lax', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (null, 20, 'F', 'brown', 'aggro', 0);
